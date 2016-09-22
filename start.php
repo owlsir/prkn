993f29c3d76060145a41d4656c5a4972bce64657
owlsir <lixudongk@163.com>
2016-09-22 01:58:19 -0800

    Modify the autoload file as an absolute path

--- start.php
+++ start.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once "./vendor/autoload.php";
+require_once __DIR__."/vendor/autoload.php";
 
-const CONFIG_DIRECTORY = __DIR__.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR;
+const CONFIG_DIRECTORY = __DIR__.'/config/';
 
 use \Prkn\Core\Prkn;
 
